102720656be6f0cf8d7d49cfc8dc9fc59cd5b9b7
Navaneeth <navaneethsaj@gmail.com>
2018-01-05 22:09:31 +0530

    Bug fixs splash screen internet access

--- app/src/main/java/com/agni/asus/quiz/SplashScreen.java
+++ app/src/main/java/com/agni/asus/quiz/SplashScreen.java
@@ -1,12 +1,18 @@
 package com.agni.asus.quiz;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.daasuu.cat.CountAnimationTextView;
+
+import es.dmoral.toasty.Toasty;
 
 public class SplashScreen extends AppCompatActivity {
     CountAnimationTextView load_counter;
@@ -30,13 +36,17 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 if (sharedPreferences.getBoolean(isfirsttime,true)){
-                    Intent i = new Intent(SplashScreen.this, FirstTime.class);
-                    editor.putString(score_key,"0");
-                    editor.putString(wrong_key,"0");
-                    editor.putString(correct_key,"0");
-                    editor.commit();
-                    startActivity(i);
-                    finish();
+                    if (haveNetworkConnection()){
+                        Intent i = new Intent(SplashScreen.this, FirstTime.class);
+                        editor.putString(score_key,"0");
+                        editor.putString(wrong_key,"0");
+                        editor.putString(correct_key,"0");
+                        editor.commit();
+                        startActivity(i);
+                        finish();
+                    }else {
+                        Toasty.error(getApplicationContext(),"No Internet Connection", Toast.LENGTH_LONG,true).show();
+                    }
                 }else {
                     Intent i = new Intent(SplashScreen.this, MainActivity.class);
                     startActivity(i);
@@ -45,5 +55,21 @@ public class SplashScreen extends AppCompatActivity {
                 }
             }
         }, 1700);
+    }
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
     }
 }
